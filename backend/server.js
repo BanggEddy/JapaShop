a7cb9ae2369b2773f5209d8488a4a02ae943ac22
David LI <davidli4532@gmail.com>
2023-02-13 14:22:46 +0100

    presentation finish back end need check

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+const routesUrls = require('./routes')
+const cors = require('cors')
+
+mongoose.set("strictQuery", false);
+
+dotenv.config()
+
+mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("Database connected"))
+
+app.use(express.json())
+app.use(cors());
+app.use('/app', routesUrls) // www.../app/signin
+app.listen(4000, () => console.log("Server 4000 up"));
+
