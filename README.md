f9c7a8357ff9342cb93682cc4901b8fefb5635ce
David LI <davidli4532@gmail.com>
2023-02-13 14:45:44 +0100

    real push

---
+++ README.md
@@ -0,0 +1,5 @@
+# Portfolio
+
+Un Portfolio que j'ai fait lors de mon stage de deuxième année avec le framework ReactJS.
+
+Il est inspiré du Portfolio en HTML de celui de base en ligne: banggeddy.github.io/
---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img src="./favicon.gif" alt="logo react"></img>
+      JapaShop
+    </div>
+  );
+};
+
+export default Logo;
