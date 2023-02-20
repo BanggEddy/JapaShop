c3acb8a7344f86ea282f5dac16eb261ea10f8c24
David LI <davidli4532@gmail.com>
2023-02-20 12:22:31 +0100

    frontend admintruepages finish

--- src/components/Admintruepage.js
+++ src/components/Admintruepage.js
@@ -3,7 +3,17 @@ import React from 'react';
 const Admintruepage = () => {
     return (
         <div>
-            
+            <div className="container">
+                <div className="centrer">
+                <p><d><b>Bienvenue dans la page Admin !
+                    </b></d></p>
+                    <br></br>
+                <p>Ici vous pouvez ajouter, supprimer où bien même modifier un article à votre guise !</p>
+<br></br>
+                <img src="./admintruefront.gif" style={{width:"70%"}}></img>
+                </div>
+
+            </div>
         </div>
     );
 };
